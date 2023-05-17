9183239451f8fa9cb1284bcdecfe1a7b891641f7
Noor Hashem <noor.hashem@applyboard.com>
2023-05-17 09:30:36 -0400

    feat: implementing price

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const cors = require('cors');
 const routes = require('./src/product/product.routes');
 const authMiddleware = require('./src/middleware/auth.middleware');
 
-const port = 8080;
+const port = 8081;
 
 const init = () => {
-    app.use(cors());
-    app.use(routes);
-    app.use(authMiddleware);
-    return app.listen(port, () => console.log(`Provider API listening on port ${port}...`));
+  app.use(cors());
+  app.use(routes);
+  app.use(authMiddleware);
+  return app.listen(port, () =>
+    console.log(`Provider API listening on port ${port}...`)
+  );
 };
 
 init();
