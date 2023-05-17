a1bf1a76c5a05d69db6a800857d818336784e4c8
Noor Hashem <noor.hashem@applyboard.com>
2023-05-17 10:31:56 -0400

    add color

--- src/product/product.repository.js
+++ src/product/product.repository.js
@@ -3,9 +3,12 @@ const Product = require('./product');
 class ProductRepository {
   constructor() {
     this.products = new Map([
-      ['09', new Product('09', 'CREDIT_CARD', 'Gem Visa', 'v1', '5')],
-      ['10', new Product('10', 'CREDIT_CARD', '28 Degrees', 'v1', '5')],
-      ['11', new Product('11', 'PERSONAL_LOAN', 'MyFlexiPay', 'v2', '5')],
+      ['09', new Product('09', 'CREDIT_CARD', 'Gem Visa', 'v1', '5', 'blue')],
+      ['10', new Product('10', 'CREDIT_CARD', '28 Degrees', 'v1', '5', 'blue')],
+      [
+        '11',
+        new Product('11', 'PERSONAL_LOAN', 'MyFlexiPay', 'v2', '5', 'blue'),
+      ],
     ]);
   }
 
